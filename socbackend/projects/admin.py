bec7c6c31e1ecab7443d82c3c09cc8bc25879283
Pratyaksh Bhardwaj <pratyakshb349@gmail.com>
2024-10-09 01:49:27 +0530

    Initial commit with project files

---
+++ socbackend/projects/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from .models import Project, Season, Mentee, MenteePreference, MenteeWishlist
+
+
+# class MentorInline(admin.TabularInline):
+#     model = MentorRequest
+#     max_num = 4
+#     extra = 1
+
+
+# class ProjectAdmin(admin.ModelAdmin):
+#     model = Project
+#     inlines = [MentorInline]
+
+
+# Register your models here.
+admin.site.register(Season)
+# admin.site.register(Project, ProjectAdmin)
+# admin.site.register(Mentor)
+admin.site.register(Project)    
+admin.site.register(Mentee)
+# admin.site.register(ProjectCategory)
+admin.site.register(MenteePreference)
+admin.site.register(MenteeWishlist)
